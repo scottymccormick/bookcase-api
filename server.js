3675cf8911fd81bea740d459d910e8ff6d488c3e
Scott McCormick <scott.t.mccormick@gmail.com>
2019-05-09 13:51:37 -0700

    add initial index route.

--- server.js
+++ server.js
@@ -2,6 +2,10 @@ const express = require('express')
 const app     = express()
 const PORT    = 3001
 
+app.get('/', (req, res) => {
+  console.log('Home route accessed')
+  res.send('Reached home route')
+})
 
 app.listen(PORT, () => {
   console.log(`Listening on ${PORT}`)

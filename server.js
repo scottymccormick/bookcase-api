caf0333331e14325d45821c1edf00bc628cdd6f5
Scott McCormick <scott.t.mccormick@gmail.com>
2019-05-13 11:03:41 -0700

    install pg, implement bodyparser.

--- server.js
+++ server.js
@@ -1,10 +1,16 @@
-const express = require('express')
-const app     = express()
-const PORT    = 3001
+const express    = require('express')
+const bodyParser = require('body-parser')
+const app        = express()
+const PORT       = 3001
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: true}))
 
 app.get('/', (req, res) => {
   console.log('Home route accessed')
-  res.send('Reached home route')
+  res.json({
+    info: 'Reached home route'
+  })
 })
 
 app.listen(PORT, () => {
